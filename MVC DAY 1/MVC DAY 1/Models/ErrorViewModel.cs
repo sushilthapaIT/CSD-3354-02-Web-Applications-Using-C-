f3943d175f8e3830868cfc73b4cf0079d0145807
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-09-13 22:28:17 -0400

    MVC c#
    Installation of MVC Template in Visual Studio c#

---
+++ MVC DAY 1/MVC DAY 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_DAY_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
